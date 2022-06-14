c55f598f53c518a39e02cfa33e0baa4dd24768ba
jellevanderwerff <jelle.vanderwerff@mpi.nl>
2022-06-14 10:49:03 +0200

    Loads of changes; added class methods for making a sequence from note values, added plot_notes, improved random_metrical_sequence etc.

--- stimulus/metrical.py
+++ stimulus/metrical.py
@@ -51,67 +51,9 @@ def random_metrical_sequence(n_bars, allowed_note_values, time_signature, quarte
 
     for bar in range(n_bars):
         ratios = random.choice(_all_metrical_ratios(allowed_note_values, time_signature))
+
         iois = np.concatenate((iois, np.round(ratios * quarternote_ms * time_signature[1])), axis=0)
 
-    return Sequence(iois, metrical=True)
-
-
-def metrical_sequence(ratios, time_signature, quarternote_ms):
-    """
-    This function should return a Sequence object given a list of ratios etc.
-    """
-
-    ratios = np.array(ratios)
-
-    if np.sum(ratios) % (time_signature[0]/time_signature[1]) != 0:
-        warnings.warn("The provided ratios do not result in a sequence with only whole bars.")
-
-    iois = np.round(ratios * quarternote_ms * time_signature[1])
-
-    return Sequence(iois, metrical=True)
-
-
-def iois_to_ratios(iois, time_signature, quarternote_ms):
-    iois = np.array(iois)
-
-    return iois / quarternote_ms / time_signature[1]
-
-
-def iois_to_notevalues(iois, time_signature, quarternote_ms):
-    iois = np.array(iois)
-    ratios = iois / quarternote_ms / time_signature[1]
-
-    note_values = np.array([1 // ratio for ratio in ratios])
-
-    return note_values
-
-
-def plot_note_values(filename, note_values, time_signature):
-
-    b = Bar(meter=time_signature)
-
-    for note_value in note_values:
-        b.place_notes('G-4', note_value)
-
-    lp = lilypond.from_Bar(
-        b) + '\n\paper {\nindent = 0\mm\nline-width = 110\mm\noddHeaderMarkup = ""\nevenHeaderMarkup = ""\noddFooterMarkup = ""\nevenFooterMarkup = ""\n}'
-
-    lilypond.save_string_and_execute_LilyPond(lp, filename, '-dbackend=eps -dresolution=600 --png -s')
-
-    filenames = ['-1.eps', '-systems.count', '-systems.tex', '-systems.texi']
-    filenames = [filename[:-4] + x for x in filenames]
-
-    for file in filenames:
-        os.remove(file)
-
-    img = mpimg.imread(filename)
-    plt.imshow(img)
-    plt.axis('off')
-    plt.show()
-
-
-
-
-
+    return Sequence(iois, metrical=True, time_sig=time_signature, quarternote_ms=quarternote_ms, n_bars=n_bars)
 
 
--- stimulus/tests.py
+++ stimulus/tests.py
@@ -1,13 +1,8 @@
 from stimulus import *
-from mingus.core.scales import Major
-from mingus.containers import Note, Bar
-from mingus.extra import lilypond
-import random
-import numpy as np
 
-ran_seq = random_metrical_sequence(1, [4, 8, 16], (4, 4), 500)
-note_values = iois_to_notevalues(ran_seq.iois, (4, 4), 500)
+seq = random_metrical_sequence(10, [2, 4, 8, 16], (4, 4), 500)
 
-plot_note_values('./ritme.png', note_values, (4, 4))
+seq.plot_notes()
 
+# They're the same bars twice :p
 
