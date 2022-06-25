a7ace00b763afd1c0b29d6af717f3230c48d7360
jellevanderwerff <jelle.vanderwerff@mpi.nl>
2022-06-25 11:37:28 +0200

    Added support for third layer

--- stimulus/lilypond_tests.py
+++ stimulus/lilypond_tests.py
@@ -1,25 +1,30 @@
 from stimulus import *
 from mingus.extra import lilypond
 
-rhythm = Rhythm.from_note_values([4] * 12)
+rhythm = Rhythm.from_note_values([4] * 4)
 stim = Stimulus.generate(freq=440, offramp=10)
 stims = Stimuli.from_stim(stim, repeats=len(rhythm.onsets))
 trial = RhythmTrial(rhythm, stims)
 trial.add_layer(rhythm, stims)
+rhythm2 = Rhythm.from_note_values([16] * 16)
+stims2 = Stimuli.from_stim(stim, repeats=len(rhythm2.onsets))
+trial.add_layer(rhythm2, stims2)
 
 layers_list = []
-layers_dict = {0: 'bd', 1: 'snare'}
+layers_dict = {0: 'bd', 1: 'snare', 2: 'hihat'}
 
-if trial.n_layers > 2:
-    raise ValueError("Can only do two layers unfortunately.")
+if trial.n_layers > 3:
+    raise ValueError("Can only do three layers unfortunately.")
 
 for layer in range(trial.n_layers):
     bars = []
     events = [event for event in trial.events if event.layer == layer]
+    print(events)
     bar = ''
     b = Bar(meter=trial.time_sig)
     for event in events:
         note_value = int(event.note_value)
+        print(note_value)
         b.place_rest(note_value)
         note = layers_dict[layer] + str(note_value) + ' '
         bar += note
@@ -31,8 +36,9 @@ for layer in range(trial.n_layers):
     layers_list.append(bars)
 
 
-voice_names = ['voiceOne', 'voiceTwo']
-layer_names = ['down', 'up']
+
+voice_names = ['voiceOne', 'voiceTwo', 'voiceThree']
+layer_names = ['uno', 'dos', 'tres']
 
 string_firstbit = ''
 string_secondbit = '\\new DrumStaff << '
@@ -48,4 +54,3 @@ for layer_i in range(len(layers_list)):
 string_secondbit += ' >>'
 
 out_string = string_firstbit + string_secondbit
-print(out_string)
