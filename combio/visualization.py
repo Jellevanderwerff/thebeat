9b49c3eca3e544ae85f437d271ac90030508bec9
jellevanderwerff <jelle.vanderwerff@mpi.nl>
2022-09-21 15:40:38 +0200

    improved acf example

--- combio/visualization.py
+++ combio/visualization.py
@@ -107,8 +107,8 @@ def plot_multiple_sequences(sequences: Iterable,
     Plot multiple sequences in one plot. Either pass it an iterable (e.g. list) of Sequence objects,
     StimSequence objects, or iterables of event onsets (so iterable of iterables, e.g. list of lists).
 
-    If the total duration is shorter than 10 seconds, the x axis values are in milliseconds. If the total duration is longer
-    than 10 seconds, the x axis values are in seconds.
+    If the total duration is shorter than 10 seconds, the x axis values are in milliseconds. If the total duration is
+    longer than 10 seconds, the x axis values are in seconds.
 
     Parameters
     ----------
