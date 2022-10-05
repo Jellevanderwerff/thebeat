e4c874613697dc286cb3de975ec3387fa21ce53d
jellevanderwerff <jelle.vanderwerff@mpi.nl>
2022-10-05 14:10:26 +0200

    add melody tests

--- tests/test_melody.py
+++ tests/test_melody.py
@@ -1,7 +1,13 @@
 import combio.melody
+import combio.rhythm
+import numpy as np
 
 
 def test_melody():
     mel = combio.melody.Melody.generate_random_melody(n_bars=2, key='G', octave=5)
-    fig, ax = mel.plot_melody()
+    fig, ax = mel.plot_melody(suppress_display=True)
     assert fig, ax
+
+    r = combio.rhythm.Rhythm.from_note_values([4, 4, 4, 4])
+    m = combio.melody.Melody.from_notes(r, 'CEGC', octave=3)
+    assert list(m.get_note_values) == [4, 4, 4, 4]
