76490229ec7e02cb66f77abd59f335676610a31a
jellevanderwerff <jellevanderwerff@gmail.com>
2024-12-13 17:17:19 +0100

    update baseline images

--- tests/test_rhythm.py
+++ tests/test_rhythm.py
@@ -86,6 +86,20 @@ def test_rhythm_plot():
     return fig
 
 
+@pytest.custom_mpl_image_compare(tolerance=2)
+def test_note_ties():
+    r1 = thebeat.music.Rhythm.from_note_values([1/4, 2, 3/4], beat_ms=500, time_signature=(4, 4))
+    fig, ax = r1.plot_rhythm()
+    return fig
+
+
+@pytest.custom_mpl_image_compare(tolerance=2)
+def test_note_ties2():
+    r2 = thebeat.music.Rhythm.from_note_values([1/16, 1/16, 1/16, 5/16, 5/16, 1/16, 1/16, 1/16], beat_ms=500, time_signature=(4, 4))
+    fig, ax = r2.plot_rhythm()
+    return fig
+
+
 def test_copy():
     r = thebeat.music.Rhythm([500, 500, 500, 500], name="test")
     r2 = r.copy()
