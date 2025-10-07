db8f6eadeea7a1ffba3c3c41280f6acfb25c4ed5
Yannick Jadoul <yannick.jadoul@belgacom.net>
2025-10-07 22:58:35 +0200

    Move rhythm_to_binary and sequence_to_binary from helpers to utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with thebeat.  If not, see <https://www.gnu.org/licenses/>.
 
+from fractions import Fraction
+
 import numpy as np
 import pytest
 
@@ -44,3 +46,60 @@ def test_concatenate_rhythms():
     r5 = thebeat.music.Rhythm.from_note_values([1/4, 1/4, 1/4, 1/4], beat_ms=250)
     with pytest.raises(ValueError, match=r"Provided rhythms should have same tempo \(beat_ms\)"):
         thebeat.utils.concatenate_rhythms([r1, r5])
+
+
+def test_sequence_to_binary():
+    seq = thebeat.core.Sequence([500, 1000, 250, 500, 250], end_with_interval=False)
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=1)
+    assert len(binary) == 2501
+    assert list(np.flatnonzero(binary)) == [0, 500, 1500, 1750, 2250, 2500]
+
+    seq = thebeat.core.Sequence([500, 1000, 250, 500, 250], end_with_interval=True)
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=1)
+    assert len(binary) == 2500
+    assert list(np.flatnonzero(binary)) == [0, 500, 1500, 1750, 2250]
+
+    seq = thebeat.core.Sequence([500, 1000, 250, 500, 250], first_onset=1000, end_with_interval=False)
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=1)
+    assert len(binary) == 3501
+    assert list(np.flatnonzero(binary)) == [1000, 1500, 2500, 2750, 3250, 3500]
+
+    seq = thebeat.core.Sequence([500, 1000, 250, 500, 250], first_onset=1000, end_with_interval=True)
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=1)
+    assert len(binary) == 3500
+    assert list(np.flatnonzero(binary)) == [1000, 1500, 2500, 2750, 3250]
+
+
+def test_sequence_to_binary_resolution():
+    seq = thebeat.core.Sequence([500, 1000, 250, 500, 250], end_with_interval=False)
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=1)
+    assert len(binary) == 2501
+    assert list(np.flatnonzero(binary)) == [0, 500, 1500, 1750, 2250, 2500]
+
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=0.5)
+    assert len(binary) == 5001
+    assert list(np.flatnonzero(binary)) == [0, 1000, 3000, 3500, 4500, 5000]
+
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=2)
+    assert len(binary) == 1251
+    assert list(np.flatnonzero(binary)) == [0, 250, 750, 875, 1125, 1250]
+
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=250)
+    assert len(binary) == 11
+    assert list(np.flatnonzero(binary)) == [0, 2, 6, 7, 9, 10]
+    assert list(binary.astype(int)) == [1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1]
+
+    binary = thebeat.utils.sequence_to_binary(seq, resolution=3)
+    assert len(binary) == 834
+    assert list(np.flatnonzero(binary)) == [0, 166, 500, 583, 750, 833]
+
+
+def test_rhythm_to_binary():
+    # Should raise error because there are 1/8th notes but the provides smallest note value is a 1/4th note
+    with pytest.raises(ValueError):
+        rhythm = thebeat.music.Rhythm.from_note_values([1/4, 1/8, 1/8, 1/4, 1/4])
+        print(thebeat.utils.rhythm_to_binary(rhythm, smallest_note_value=Fraction(1, 4)))
+
+    # Should not raise error
+    binary = thebeat.utils.rhythm_to_binary(rhythm, smallest_note_value=Fraction(1, 8))
+    assert np.all(binary == [1., 0., 1., 1., 1., 0., 1., 0.])
